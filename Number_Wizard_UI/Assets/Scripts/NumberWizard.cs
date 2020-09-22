a6cf8170bf00a350d9ec265678e4ffaa20fd310d
Marcel Kołodziej <kolodziejmarcel1@gmail.com>
2020-09-22 20:08:42 +0200

    Setup scenes and script

---
+++ Number_Wizard_UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            max = guess;
+            NextGuess();
+        }
+    }
+
+    public void OnPressLower() {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            min = guess;
+            NextGuess();
+        }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than..." + guess);
+    }
+}
+
